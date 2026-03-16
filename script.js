b1ad4b84308eb0e8803d845882b6ba4719f2e3c2
damari-cmd <damarim006@gmail.com>
2026-03-16 12:15:26 -0400

    test2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Initialization
-const SB_URL = https//tqegziwxeqpfypnyphar.supabase.co
+const SB_URL = "https://tqegziwxeqpfypnyphar.supabase.co";
 const SB_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxZWd6aXd4ZXFwZnlwbnlwaGFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTc5ODAsImV4cCI6MjA4ODM5Mzk4MH0.zTiY9nwnVBqs9Btc_aJjDZian7WK02VhxnojJQq36uI
 const supabaseClient = supabase.createClient(SB_URL, SB_KEY);
 
